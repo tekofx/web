1cb2b5d247e0f460561b80d825ef1f2728ee3636
tekofx <tekofxx@gmail.com>
2022-03-08 18:14:18 +0100

    Created pages

---
+++ src/pages/Gallery.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Gallery extends React.Component {
+    render() {
+        return (
+            <h1>Gallery</h1>
+
+        )
+    }
+}
+
+export default Gallery;

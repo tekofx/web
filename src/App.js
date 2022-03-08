134fb5c546dd5c4adc378ed8338492a034de6836
tekofx <tekofxx@gmail.com>
2022-03-08 18:20:21 +0100

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Navbar from './Navbar';
+import Navbar from './/components/Navbar/Navbar';
 import Home from './pages/Home';
 function App() {
   return (

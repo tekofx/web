414f05e51beffd30a5cc43c4d6b818accd9764dc
tekofx <tekofxx@gmail.com>
2022-03-08 18:13:24 +0100

    Moved header to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import Header from './Header';
+import Header from './components/Header/Header';
 import Navbar from './Navbar';
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <Home />
+
     </div>
   );
 }

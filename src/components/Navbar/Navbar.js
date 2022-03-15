6750d46976b828649953c8ac8fbefd60ad813e9a
tekofx <tekofxx@gmail.com>
2022-03-15 18:41:30 +0100

    Navbar fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import './Navbar.css';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 class Navbar extends React.Component {
   render() {
     return (
       <nav>
-        <NavLink className={({ isActive }) => (isActive ? "linkActive" : "link")} to="/home" >Home</NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "linkActive" : "link")} to="/about">About Me</NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "linkActive" : "link")} to="/gallery">Gallery</NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "linkActive" : "link")} to="/contact">Contact</NavLink>
+        <NavLink className={({ isActive }) => (isActive ? "link active" : "link")} to="/home" >Home</NavLink>
+        <NavLink className={({ isActive }) => (isActive ? "link active" : "link")} to="/about">About Me</NavLink>
+        <NavLink className={({ isActive }) => (isActive ? "link active" : "link")} to="/gallery">Gallery</NavLink>
+        <NavLink className={({ isActive }) => (isActive ? "link active" : "link")} to="/contact">Contact</NavLink>
 
       </nav>
 

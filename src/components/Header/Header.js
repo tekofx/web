11fbb6c73f6f771aaae4e53836a2b4fe0b028a04
tekofx <tekofxx@gmail.com>
2022-03-15 18:18:11 +0100

    Navbar working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
 import './Header.css'
-class Header extends React.Component {
-    render() {
-        return (
-            <img src="/header.jpg" alt="" />
+import Navbar from '../Navbar/Navbar'
 
-        )
-    }
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <img src="/header.jpg" alt="" />
+        <Navbar />
+      </>
+
+
+    )
+  }
 }
 
 export default Header;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,21 @@
 import React from "react";
 import './Navbar.css';
+import { Link } from 'react-router-dom';
+
 
 class Navbar extends React.Component {
-    render() {
-        return (
-            <ul>
-                <li>Home</li>
-                <li>About Me</li>
-                <li>Gallery</li>
-                <li>Contact</li>
+  render() {
+    return (
+      <ul>
+        <li><Link to="/home">Home</Link> </li>
+        <li><Link to="/about">About Me</Link></li>
+        <li><Link to="/gallery">Gallery</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
 
-            </ul>
+      </ul>
 
-        )
-    }
+    )
+  }
 }
 
 export default Navbar;

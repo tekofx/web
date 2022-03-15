25312e509ac7eed4d414be40c0dc3905eb7a0ee3
tekofx <tekofxx@gmail.com>
2022-03-15 19:48:15 +0100

    Changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import "./theme.css"
+import avatar from '../img/avatar.png';
 
 class About extends React.Component {
   render() {
@@ -8,6 +9,10 @@ class About extends React.Component {
       <>
         <Header />
         <h1>About</h1>
+        <div className="about">
+          <img className="avatar" src={avatar} alt="" />
+          <p>I'm Teko an arctic foxxo from Spain. I love drawing, taking photos and programming. I started this webpage to challenge myself into creating a webpage and for having all my projects and stuff in one site.</p>
+        </div>
       </>
 
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import "./theme.css"
+import "./theme.css";
 
 
 class Contact extends React.Component {
@@ -9,33 +9,7 @@ class Contact extends React.Component {
       <>
         <Header />
         <h1>Social Networks</h1>
-        <div class="block_social_networks">
-          <div class="social_network">
-            <a target="_blank" href="https://github.com/Tekofx"><img class="social_logo" src="img/github.svg" alt="" /></a>
-            <h2 class="social_text">Github</h2>
-            <p class="social_text">Here I have all my tech related projects like the repo of this website</p>
-          </div>
-          <div class="social_network">
-            <a target="_blank" href="https://twitter.com/Teko_fx"><img class="social_logo" src="img/twitter.svg" alt="" /></a>
-            <h2 class="social_text">Twitter</h2>
-            <p class="social_text">The social network that I use the most and where I RT to art and furry things</p>
-          </div>
-          <div class="social_network">
-            <a target="_blank" href="https://www.instagram.com/teko_fx/"><img class="social_logo" src="img/instagram.svg" alt="" /></a>
-            <h2 class="social_text">Instagram</h2>
-            <p class="social_text">I'm not very active but I post my drawings once in a while</p>
-          </div>
-          <div class="social_network">
-            <a target="_blank" href="https://www.furaffinity.net/user/teko./"><img class="social_logo" src="img/furaffinity.svg" alt="" /></a>
-            <h2 class="social_text">Furaffinity</h2>
-            <p class="social_text">The biggest furry social network, but I'm not too much active in there</p>
-          </div>
-          <div class="social_network">
-            <a target="_blank" href="https://www.deviantart.com/tekofx"><img class="social_logo" src="img/deviantart.svg" alt="" /></a>
-            <h2 class="social_text">DeviantArt</h2>
-            <p class="social_text">The best place to see my photographies and drawings</p>
-          </div>
-        </div>
+
 
 
       </>

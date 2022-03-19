4de372de96be3551173de9efc90ed57619c10e63
tekofx <tekofxx@gmail.com>
2022-03-19 20:31:23 +0100

    Minor look changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import "./theme.css"
+import Avatar from '@mui/material/Avatar';
 import avatar from '../img/avatar.png';
 
 class About extends React.Component {
@@ -10,8 +11,8 @@ class About extends React.Component {
         <Header />
         <h1>About</h1>
         <div className="about">
-          <img className="avatar" src={avatar} alt="" />
-          <p>I'm Teko an arctic foxxo from Spain. I love drawing, taking photos and programming. I started this webpage to challenge myself into creating a webpage and for having all my projects and stuff in one site.</p>
+          <Avatar alt="Remy Sharp" src={avatar} sx={{ width: "20%", height: "20%" }} />
+          <p className="paragraph">I'm Teko an arctic foxxo from Spain. I love drawing, taking photos and programming. I started this webpage to challenge myself into creating a webpage and for having all my projects and stuff in one site.</p>
         </div>
       </>
 

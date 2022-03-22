a88066a8ad21351dd696d2e232c4ca11e4c139b2
tekofx <tekofxx@gmail.com>
2022-03-23 00:01:22 +0100

    Removed import

--- src/components/Github/Repository/Repository.js
+++ src/components/Github/Repository/Repository.js
@@ -5,7 +5,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { createTheme } from '@mui/material';
 import './Repository.css';
 import { ThemeProvider } from '@emotion/react';
 import Theme from '../../Theme/Theme';

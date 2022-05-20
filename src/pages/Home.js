60dc4743cc699486eba6e238b2db3a046d05c7c0
tekofx <tekofxx@gmail.com>
2022-05-20 22:11:29 +0200

    Removed container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,18 +9,16 @@ class Home extends React.Component {
   render() {
     return (
       <ThemeProvider theme={Theme}>
-        <Container maxWidth="xl">
+        <Container>
           <Appbar />
           <br />
-          <Container >
-            <Typography variant="h1">Welcome</Typography>
-            <Typography>On this page I will collect all the information / projects that I am doing, most of them related to Furry Fandom</Typography>
+          <Typography variant="h1">Welcome</Typography>
+          <Typography>On this page I will collect all the information / projects that I am doing, most of them related to Furry Fandom</Typography>
 
-            <Typography variant="h2">News</Typography>
-            <Typography>- This webpage is still under construction, I will add more content as soon as possible.</Typography>
+          <Typography variant="h2">News</Typography>
+          <Typography>- This webpage is still under construction, I will add more content as soon as possible.</Typography>
 
 
-          </Container>
 
         </Container>
       </ThemeProvider>

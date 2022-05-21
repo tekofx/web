25e576271705bb331bf44563734aa6f0b067d1f4
Teko <49290914+tekofx@users.noreply.github.com>
2022-05-21 10:55:03 +0000

    Edited responsive

--- src/components/Github/RepositoryList/RepositoryList.js
+++ src/components/Github/RepositoryList/RepositoryList.js
@@ -69,10 +69,10 @@ export default function BasicGrid(props) {
     return (
         <>
             <Box sx={{ flexGrow: 1 }} >
-                <Grid container spacing={{ xs: 1, sm: 2, md: 3, lg: 4 }} columns={{ xs: 12, sm: 12, md: 12, lg: 12 }}>
+                <Grid container spacing={{ xs: 1, sm: 1, md: 3, lg: 4 }} columns={{ xs: 12, sm: 12, md: 12, lg: 10, xl: 12 }}>
                     {(loading ? Array.from(new Array(6)) : repos).map((item, index) => (
 
-                        <Grid item xs={12} sm={6} md={4} lg={3} key={index} >
+                        <Grid item xs={6} sm={4} md={3} lg={2} xl={2} key={index} >
                             {item ? (
                                 <Repository title={item.name} description={item.description} url={item.html_url} img={item.img} />
                             ) : (

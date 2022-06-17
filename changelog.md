66c904d9487240601f8a9ca1676e964297cf1e79
Teko <tekofxx@gmail.com>
2022-06-17 14:42:10 +0200

    Animation on page change

--- changelog.md
+++ changelog.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added 
 - Popup in about page
+- Animation on page change
 
 ## [v0.1.5] - 09/06/2022
 Fixed routes issues
--- src/App.js
+++ src/App.js
@@ -6,30 +6,37 @@ import Theme from './Theme';
 import { ThemeProvider } from '@mui/material/styles';
 import Appbar from './components/Appbar';
 import Footer from './components/Footer';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate
+  Navigate,
+  useLocation
 } from "react-router-dom";
 import './index.css';
 import routes from './routes.json';
 
 
 function App() {
+  const location = useLocation();
+
   return (
     <ThemeProvider theme={Theme}>
-      <Router>
-        <Appbar />
-        <Routes >
-          <Route path='/' element={<Navigate to={routes.home} />} />
-          <Route path={routes.home} element={<Home />} />
-          <Route path={routes.gallery} element={<Gallery />} />
-          <Route path={routes.projects} element={<Projects />} />
-          <Route path={routes.about} element={<About />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Appbar />
+      <TransitionGroup component={null}>
+        <CSSTransition key={location.key} classNames="fade" timeout={300}>
+          <Routes >
+            <Route path='/' element={<Navigate to={routes.home} />} />
+            <Route path={routes.home} element={<Home />} />
+            <Route path={routes.gallery} element={<Gallery />} />
+            <Route path={routes.projects} element={<Projects />} />
+            <Route path={routes.about} element={<About />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
+      <Footer />
     </ThemeProvider>
   );
 }

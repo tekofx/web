da474f94f61cb4314db2b981b50b1833c89ab527
tekofx <tekofxx@gmail.com>
2022-06-22 16:27:10 +0200

    modified translations

--- src/components/Seasons/Seasons.js
+++ src/components/Seasons/Seasons.js
@@ -1,24 +1,38 @@
 import * as React from 'react';
 import { Typography } from '@mui/material';
-import Container from '@mui/material/Container';
-import Confetti from './Confetti'
+import { useTranslation } from 'react-i18next';
+
 const pride = require('./summer - lgtb.jpg')
 
-
-
 export default function Footer() {
+    const { t } = useTranslation();
 
     const month = new Date().getMonth();
+    const day = new Date().getDay();
 
     switch (month) {
         case 5:
             return (
                 <div >
-                    <Typography>Happy Pride!</Typography>
+                    <Typography>{t('prideText')}</Typography>
                     {/* <Confetti className="particles" /> */}
-                    <img src={pride} width='50%' />
+                    <img alt="" src={pride} width='50%' />
                 </div >
             )
+            break;
+
+        case 9:
+            if (day === 31) {
+                return (
+                    <div >
+                        <Typography>{t('halloweenText')}</Typography>
+                        {/* <Confetti className="particles" /> */}
+                        <img alt="" src={pride} width='50%' />
+                    </div >
+                )
+
+            }
+            break
 
 
         default:

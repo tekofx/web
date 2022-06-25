245a54919d027ce5ce8661924dbdae81c6e42a54
Teko <49290914+tekofx@users.noreply.github.com>
2022-06-25 15:21:34 +0000

    Added halloween and christmas

--- src/components/Seasons/Seasons.js
+++ src/components/Seasons/Seasons.js
@@ -3,12 +3,16 @@ import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 const pride = require('./summer - lgtb.jpg')
+const halloween = require('./juaguelin.png')
+const christmas = require('./Merry Foxmas.png')
+
 
 export default function Footer() {
     const { t } = useTranslation();
 
-    const month = new Date().getMonth();
-    const day = new Date().getDay();
+    const today = new Date();
+    const month = today.getMonth();
+    const day = parseInt(String(today.getDate()).padStart(2, '0'));
 
     switch (month) {
         case 5:
@@ -16,7 +20,7 @@ export default function Footer() {
                 <div >
                     <Typography>{t('prideText')}</Typography>
                     {/* <Confetti className="particles" /> */}
-                    <img alt="" src={pride} width='50%' />
+                    <img alt="" src={pride} width='100%' />
                 </div >
             )
             break;
@@ -27,7 +31,20 @@ export default function Footer() {
                     <div >
                         <Typography>{t('halloweenText')}</Typography>
                         {/* <Confetti className="particles" /> */}
-                        <img alt="" src={pride} width='50%' />
+                        <img alt="" src={halloween} width='100%' />
+                    </div >
+                )
+
+            }
+            break
+
+        case 11:
+            if (23 < day < 30) {
+                return (
+                    <div >
+                        <Typography>{t('christmasText')}</Typography>
+                        {/* <Confetti className="particles" /> */}
+                        <img alt="" src={christmas} width='100%' />
                     </div >
                 )
 

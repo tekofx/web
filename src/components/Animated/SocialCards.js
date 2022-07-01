53cba5c0c1ffa0141a75ad2f4573ac0f107a45d7
tekofx <tekofxx@gmail.com>
2022-07-01 13:19:12 +0200

    Added username

--- src/components/Animated/SocialCards.js
+++ src/components/Animated/SocialCards.js
@@ -11,25 +11,25 @@ export default function SocialCards() {
     return (
         <Grid container spacing={1} >
             <Grid item xs={6} sm={4} md={3} lg={2}>
-                <AnimatedCard icon={<FaTwitter />} content="Twitter" subContent="Twitter" url="https://twitter.com/teko_fx" />
+                <AnimatedCard icon={<FaTwitter />} content="Twitter" subContent="@Teko_fx" url="https://twitter.com/teko_fx" />
             </Grid>
             <Grid item xs={6} sm={4} md={3} lg={2}>
-                <AnimatedCard icon={<FaMastodon />} content="Mastodon" subContent="Twitter" url="https://meow.social/@teko" />
+                <AnimatedCard icon={<FaMastodon />} content="Mastodon" subContent="meow.social@teko" url="https://meow.social/@teko" />
             </Grid>
             <Grid item xs={6} sm={4} md={3} lg={2}>
-                <AnimatedCard icon={<FaGithub />} content="Github" subContent="Twitter" url="https://github.com/tekofx" />
+                <AnimatedCard icon={<FaGithub />} content="Github" subContent="tekofx" url="https://github.com/tekofx" />
             </Grid>
             <Grid item xs={6} sm={4} md={3} lg={2}>
-                <AnimatedCard icon={<FaDeviantart />} content="DeviantArt" subContent="Twitter" url="https://www.deviantart.com/tekofx" />
+                <AnimatedCard icon={<FaDeviantart />} content="DeviantArt" subContent="tekofx" url="https://www.deviantart.com/tekofx" />
             </Grid>
             <Grid item xs={6} sm={4} md={3} lg={2}>
-                <AnimatedCard icon={<Fa500Px />} content="500px" subContent="Twitter" url="https://500px.com/p/tekofx" />
+                <AnimatedCard icon={<Fa500Px />} content="500px" subContent="tekofx" url="https://500px.com/p/tekofx" />
             </Grid>
             <Grid item xs={6} sm={4} md={3} lg={2}>
-                <AnimatedCard icon={<FaInstagram />} content="Instagram" subContent="Twitter" url="https://www.instagram.com/teko_fx/" />
+                <AnimatedCard icon={<FaInstagram />} content="Instagram" subContent="teko_fx" url="https://www.instagram.com/teko_fx/" />
             </Grid>
             <Grid item xs={6} sm={4} md={3} lg={2}>
-                <AnimatedCard icon={<FurAffinity />} content="FurAffinity" subContent="Twitter" url="https://www.furaffinity.net/user/teko." />
+                <AnimatedCard icon={<FurAffinity />} content="FurAffinity" subContent="teko." url="https://www.furaffinity.net/user/teko." />
             </Grid>
         </Grid>
     );

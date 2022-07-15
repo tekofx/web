052787a9782ae9ec0a8452690acb7f648fe5945b
tekofx <tekofxx@gmail.com>
2022-07-15 20:14:25 +0200

    Changed theme and commands

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -4,15 +4,32 @@ import { ReactTerminal } from "react-terminal";
 
 export default function Terminal() {
     const commands = {
-        whoami: "jackharper",
-        cd: (directory) => `changed path to ${directory}`
+        cd: (directory) => `changed path to ${directory}`,
+        ls: "uwu owo ewe ",
+        help: (<span>
+            <strong>clear</strong> - clears the console. <br />
+            <strong>ls</strong> - shows folder. <br />
+        </span>),
     };
+
+    const themes = {
+        default: {
+            themeBGColor: "#272B36",
+            themeToolbarColor: "#DBDBDB",
+            themeColor: "#FFFEFC",
+            themePromptColor: "#a917a8"
+        }
+    }
 
     return (
         < TerminalContextProvider >
 
             <ReactTerminal
                 commands={commands}
+                themes={themes}
+                theme="default"
+                welcomeMessage={<span>Type help for available commands<br /></span>}
+                showControlBar={false}
             />
         </TerminalContextProvider >
     )

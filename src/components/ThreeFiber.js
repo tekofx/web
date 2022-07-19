85e365cdfe3992bbd186869daeb3797680c9cbaf
tekofx <tekofxx@gmail.com>
2022-07-19 21:00:23 +0200

    Removed unused imports

--- src/components/ThreeFiber.js
+++ src/components/ThreeFiber.js
@@ -1,40 +1,37 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stars } from '@react-three/drei'
-import { Container } from '@mui/material'
-import { Paper } from '@mui/material'
-
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import { Paper } from "@mui/material";
 
 function Box() {
-    return (
-        <mesh position={[0, 1, 0]}>
-            <boxBufferGeometry attach="geometry" />
-            <meshLambertMaterial attach="material" color="hotpink" />
-        </mesh>
-    )
+  return (
+    <mesh position={[0, 1, 0]}>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
 }
 
 function Plane() {
-    return (
-        <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-            <planeBufferGeometry attach='geometry' args={[100, 100]} />
-            <meshLambertMaterial attach='material' color="lightblue" />
-
-        </mesh>
-    )
+  return (
+    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
 }
 
 export default function ThreeFiber() {
-    return (
-        <Paper sx={{ height: '400px' }}>
-            <Canvas style={{ background: "black" }}>
-                <OrbitControls autoRotate='true' />
-                <Stars />
-                <ambientLight intensity={0.5} />
-                <spotLight position={[10, 15, 10]} angle={0.3} />
-                <Box />
-                <Plane />
-            </Canvas>
-        </Paper>
-    )
+  return (
+    <Paper sx={{ height: "400px" }}>
+      <Canvas style={{ background: "black" }}>
+        <OrbitControls autoRotate="true" />
+        <Stars />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <Box />
+        <Plane />
+      </Canvas>
+    </Paper>
+  );
 }

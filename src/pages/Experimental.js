8d026ab7d2ba867fbf124abac0790f26601687f1
tekofx <tekofxx@gmail.com>
2022-07-19 20:29:43 +0200

    Removed terminal

--- src/pages/Experimental.js
+++ src/pages/Experimental.js
@@ -1,55 +1,40 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import { Grid } from "@mui/material";
-import { useTranslation } from 'react-i18next';
-import '../index.css'
-import Container from '@mui/material/Container';
-import ThreeFiber from '../components/ThreeFiber'
-import AmongUS from '../components/AmongUS/AmongUS'
-import Spring from '../components/Spring';
-import Terminal from '../components/Terminal';
+import { useTranslation } from "react-i18next";
+import "../index.css";
+import Container from "@mui/material/Container";
+import ThreeFiber from "../components/ThreeFiber";
+import AmongUS from "../components/AmongUS/AmongUS";
+import Spring from "../components/Spring";
+import Terminal from "../components/Terminal";
 
 function Home() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <Container maxWidth='xl'>
-            <Grid container spacing={1} >
-                <Grid item xs={12} sm={12} md={12} lg={12}>
+  return (
+    <Container maxWidth="xl">
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <Typography variant="h1">{t("experimentalTitle")}</Typography>
+          <Typography variant="body1">{t("experimentalText")}</Typography>
+        </Grid>
 
-                    <Typography variant="h1">{t('experimentalTitle')}</Typography>
-                    <Typography variant='body1'>{t('experimentalText')}</Typography>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <Typography variant="h2">ThreeFiber</Typography>
+          <ThreeFiber />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <Typography variant="h2">TsParticles</Typography>
+          <AmongUS />
+        </Grid>
 
-                </Grid >
-
-                <Grid item xs={12} sm={12} md={6} lg={6}>
-                    <Typography variant='h2'>ThreeFiber</Typography>
-                    <ThreeFiber />
-
-                </Grid>
-                <Grid item xs={12} sm={12} md={6} lg={6}>
-
-                    <Typography variant='h2'>TsParticles</Typography>
-                    <AmongUS />
-
-                </Grid>
-
-                <Grid item xs={12} sm={12} md={6} lg={6}>
-                    <Typography variant='h2'>Spring</Typography>
-                    <Spring />
-
-                </Grid>
-                <Grid item xs={12} sm={12} md={6} lg={6}>
-
-                    <Typography variant='h2'>Terminal</Typography>
-                    <Container sx={{ height: '130px' }} fixed>
-
-                        <Terminal />
-                    </Container>
-
-                </Grid>
-            </Grid>
-        </Container >
-    );
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <Typography variant="h2">Spring</Typography>
+          <Spring />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 export default Home;

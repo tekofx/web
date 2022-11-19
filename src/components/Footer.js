651888299a011013a1aa1d0536db564fb60b09b0
tekofx <tekofxx@gmail.com>
2022-11-19 13:56:54 +0100

    Added secret button to experimental page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,42 @@
-import * as React from 'react';
-import { Grid } from '@mui/material';
-import { Typography } from '@mui/material';
-import SocialIcons from './SocialIcons';
+import * as React from "react";
+import { Grid } from "@mui/material";
+import { Typography } from "@mui/material";
+import SocialIcons from "./SocialIcons";
+import { useNavigate } from "react-router";
+import routes from "../routes.json";
 
 export default function Footer() {
+  const year = new Date().getFullYear();
+  const navigate = useNavigate();
 
-    const year = new Date().getFullYear();
-
-    return (
-        <Grid container spacing={2}
-            sx={{ paddingBottom: '1%', paddingTop: '1%' }}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{ paddingBottom: "1%", paddingTop: "1%" }}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item xs={12} sm={12} md={12} lg={12}>
+        <Typography display="inline" variant="body2">
+          T
+        </Typography>
+        <Typography
+          display="inline"
+          variant="body2"
+          onClick={function (event) {
+            navigate(routes.experimental);
+          }}
         >
-            <Grid item xs={12} sm={12} md={12} lg={12}>
-                <Typography display='inline' variant='body2'>Teko's Tundra {year} </Typography>
-                <SocialIcons />
-            </Grid>
-        </Grid>
-    );
-}
+          e
+        </Typography>
+        <Typography display="inline" variant="body2">
+          ko's Tundra {year}{" "}
+        </Typography>
 
+        <SocialIcons />
+      </Grid>
+    </Grid>
+  );
+}

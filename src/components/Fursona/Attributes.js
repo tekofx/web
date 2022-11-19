6ed1552f615409c7eaff939067521944fc93ebd2
tekofx <tekofxx@gmail.com>
2022-11-19 21:42:17 +0100

    Improved attributes and added language support

--- src/components/Fursona/Attributes.js
+++ src/components/Fursona/Attributes.js
@@ -8,64 +8,60 @@ import TableRow from "@mui/material/TableRow";
 import { GiBroadsword } from "react-icons/gi";
 import { BsFillLightningChargeFill, BsFillShieldFill } from "react-icons/bs";
 import { FaBrain } from "react-icons/fa";
+import { useTranslation } from "react-i18next";
+import { makeStyles } from "@mui/material";
 
 export default function Attributes() {
+  const { t } = useTranslation();
+  var style = { marginRight: "2%", verticalAlign: "middle" };
+  const values = [
+    {
+      name: t("attributesStrength"),
+      value: 1,
+      icon: <GiBroadsword style={style} />,
+    },
+    {
+      name: t("attributesSpeed"),
+      value: 4,
+      icon: <BsFillLightningChargeFill style={style} />,
+    },
+    {
+      name: t("attributesIntelligence"),
+      value: 2,
+      icon: <FaBrain style={style} />,
+    },
+    {
+      name: t("attributesDefense"),
+      value: 3,
+      icon: <BsFillShieldFill style={style} />,
+    },
+  ];
+
   return (
     <TableContainer>
       <Table size="small">
         <TableHead>
           <TableRow>
             <TableCell>
-              <Typography>Attributes</Typography>
+              <Typography>{t("attributes")}</Typography>
             </TableCell>
             <TableCell></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <TableCell>
-              <Typography variant="body1">
-                <GiBroadsword />
-                Ataque
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">1</Typography>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Typography variant="body1">
-                <BsFillLightningChargeFill />
-                Velocidad
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">4</Typography>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Typography variant="body1">
-                <FaBrain />
-                Inteligencia
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">2</Typography>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Typography variant="body1">
-                <BsFillShieldFill />
-                Defensa
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">3</Typography>
-            </TableCell>
-          </TableRow>
+          {values.map((value, key) => (
+            <TableRow key={key}>
+              <TableCell>
+                <Typography variant="body1">
+                  {value.icon}
+                  {value.name}
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body1">{value.value}</Typography>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>

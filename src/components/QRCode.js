006db3d7e9f85c701620fbd7cb9912bc210c5247
tekofx <tekofxx@gmail.com>
2022-11-25 20:59:31 +0100

    Removed

--- src/components/QRCode.js
+++
@@ -1,9 +0,0 @@
-import QRCode from "react-qr-code";
-
-export default function About(props) {
-  return (
-    <div style={{ background: "white", padding: "5%" }}>
-      <QRCode value={props.value} size={50} />
-    </div>
-  );
-}

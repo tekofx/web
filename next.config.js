ae0508d497df0dc7f05c437949abe3361a1038a2
Teko <49290914+tekofx@users.noreply.github.com>
2022-11-26 15:49:58 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: false,
-  output: "standalone",
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
 };

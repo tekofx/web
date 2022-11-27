be2ae01989528f3929c319c7d605214021884e62
tekofx <tekofxx@gmail.com>
2022-11-27 19:12:36 +0100

    changes in favicon

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 let assetPrefix = ''
 let basePath = ''
 let imagePath = ""
-process.env.FAVICON = ""
+process.env.FAVICON = "favicon.ico"
 
 if (isGithubActions) {
   assetPrefix = "/web"
   basePath = "/web"
   imagePath = "/web"
-  process.env.FAVICON = "/web"
+  process.env.FAVICON = "https://tekofx.github.io/web/favicon.ico"
 
 }
 

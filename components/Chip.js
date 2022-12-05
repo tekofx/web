9dcee6d1699b393446ca2727ae3089bb9d54f5fb
tekofx <tekofxx@gmail.com>
2022-12-05 17:59:17 +0100

    Added pointer

--- components/Chip.js
+++ components/Chip.js
@@ -3,14 +3,16 @@ import { motion } from "framer-motion";
 
 
 
-export default function Chip() {
+export default function Chip(props) {
     return (
         <motion.div
-            style={{ backgroundColor: "white", width: "15rem", height: "5rem", borderRadius: "100rem", display: "flex", justifyContent: "center", alignItems: "center" }}
+            style={{ backgroundColor: "white", width: "15rem", height: "5rem", borderRadius: "100rem", display: "flex", justifyContent: "center", alignItems: "center", cursor: "pointer" }}
             whileHover={{ scale: 1.20, backgroundColor: "white", color: "black" }}
             transition={{ type: "spring", stiffness: 400, damping: 20 }}
+            onClick={props.onClick}
+
         >
-            <Typography color={"black"} variant="body2">Chip</Typography>
+            <Typography color={"black"} variant="body2">{props.text}</Typography>
         </motion.div>
     )
 }

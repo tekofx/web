f57c49ce668999c706f926a81ab578170e637307
tekofx <tekofxx@gmail.com>
2022-12-14 18:15:20 +0100

    added paper

--- components/Presentation.js
+++ components/Presentation.js
@@ -1,4 +1,4 @@
-import { Grid, Avatar, Typography, Box, Stack } from "@mui/material";
+import { Grid, Avatar, Typography, Box, Stack, Paper } from "@mui/material";
 import Chip from "./Chip";
 import { useRouter } from "next/router";
 
@@ -6,45 +6,47 @@ export default function Presentation() {
     const router = useRouter();
 
     return (
-        <Grid container spacing={4}
-            direction="row"
-            alignItems="center"
-            justifyContent="center" >
+        <Paper sx={{ paddingLeft: 2, paddingRight: 2 }}>
+            <Grid container spacing={4}
+                direction="row"
+                alignItems="center"
+                justifyContent="center" >
 
-            <Grid item lg={12}>
-                <Box
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-
-                    <Avatar sx={{ width: 400, height: 400 }} src="img/avatar.jpg" />
-                </Box>
-                <Typography align="center" variant="h2">Hello, I'm Teko an arctic foxxo</Typography>
-                <Typography align="center" variant="h3">I love waffles</Typography>
-
-
-
-            </Grid>
-            <Grid item lg={12}>
-                <Box
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    <Stack direction="row" spacing={7}
-
+                <Grid item lg={12}>
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
                     >
-                        <Chip text="Recent Blog Posts" onClick={() => router.push("/blog")} />
-                        <Chip text="My Fursona" onClick={() => router.push("/fursona")} />
 
-                    </Stack>
-                </Box>
+                        <Avatar sx={{ width: 400, height: 400 }} src="img/avatar.jpg" />
+                    </Box>
+                    <Typography align="center" variant="h2">Hello, I'm Teko an arctic foxxo</Typography>
+                    <Typography align="center" variant="h3">I love waffles</Typography>
+
+
+
+                </Grid>
+                <Grid item lg={12}>
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        <Stack direction="row" spacing={7}
+
+                        >
+                            <Chip text="Recent Blog Posts" onClick={() => router.push("/blog")} />
+                            <Chip text="My Fursona" onClick={() => router.push("/fursona")} />
+
+                        </Stack>
+                    </Box>
+                </Grid>
+
+
+
             </Grid>
-
-
-
-        </Grid>
+        </Paper>
     )
 
 }

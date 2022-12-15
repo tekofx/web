da43966dc5add7ee4981c3b81a678d4d30b76a29
tekofx <tekofxx@gmail.com>
2022-12-15 18:07:58 +0100

    Added 404

---
+++ pages/404.js
@@ -0,0 +1,35 @@
+import { Box, Grid, Typography, Alert, Avatar } from "@mui/material"
+import { motion } from "framer-motion"
+
+export default function FourOhfour() {
+    return (
+        <Box height="1000px">
+            <Grid container justifyContent="center" alignItems="center" spacing={2}>
+
+                <Grid item xs={10} sm={10} md={8} lg={3}>
+                    <Avatar
+                        component={motion.div}
+                        alt="Remy Sharp"
+                        src="img/logo-color.png"
+                        sx={{ width: "100%", height: "auto" }}
+                        animate={{
+                            rotate: [0, 0, 360, 360, 0],
+                        }}
+                        transition={{
+                            duration: 2,
+                            ease: "easeInOut",
+                            times: [0, 0.2, 0.5, 0.8, 1],
+                            repeat: Infinity,
+                            repeatDelay: 1
+                        }}
+                    />
+
+
+                </Grid>
+                <Grid item xs={10} sm={10} md={10} lg={10}>
+                    <Alert variant="filled" severity="error">This page could not be found.</Alert>
+                </Grid>
+            </Grid>
+        </Box >
+    )
+}

1bfe0cefebfd1f5aef55817998840e4061d4d24c
tekofx <tekofxx@gmail.com>
2022-12-23 00:49:45 +0100

    Modified transition

--- components/Layout/Transition.js
+++ components/Layout/Transition.js
@@ -5,15 +5,15 @@ const variants = {
     opacity: 0,
     y: 40,
     transition: {
-      duration: 0.75,
+      duration: 0.5,
     },
   },
   in: {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 0.75,
-      delay: 0.5,
+      duration: 0.5,
+      delay: 0.2,
     },
   },
 };

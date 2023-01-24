d0302a643fbe72c016de3ba901e8373122665d43
tekofx <tekofxx@gmail.com>
2023-01-24 09:18:14 +0100

    Modified paper

--- pages/projects/cardcreator.js
+++ pages/projects/cardcreator.js
@@ -46,135 +46,133 @@ export default function CardCreator() {
     }
 
     return (
-        <div>
+        <Paper sx={{ p: 2, marginTop: 2 }}>
             <Typography variant="h1">Fursona Card Creator</Typography>
             <Typography variant="body1">
                 Create your fursona custom card and download it
             </Typography>
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={12} md={12} lg={12}>
-                    <Paper sx={{ p: 2 }}>
-                        <Grid container >
-                            <Grid item xs={12} sm={12} md={6} lg={5}>
-                                <Card
-                                    name={name}
-                                    species={species}
-                                    speciesLatin={speciesLatin}
-                                    pronouns={pronouns}
-                                    web={web}
-                                    birthday={birthday}
-                                    img={file}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={6} lg={7}>
-                                <Box
-                                    component="form"
-                                    sx={{
-                                        "& .MuiTextField-root": {
-                                            m: 1,
-                                            width: "25ch",
-                                        },
-                                    }}
-                                    noValidate
-                                    autoComplete="off"
-                                >
-                                    <Grid container spacing="2">
-                                        <Grid item xs={12} sm={6} lg={6}>
-                                            <TextField
-                                                style={{ width: "80%" }}
-                                                defaultValue={name}
-                                                required
-                                                label="Name"
-                                                variant="standard"
-                                                onChange={(e) => setName(e.target.value)}
-                                            />
-                                        </Grid>
-                                        <Grid item xs={12} sm={6} lg={6}>
-                                            <TextField
-                                                style={{ width: "80%" }}
-                                                defaultValue={species}
-                                                required
-                                                label="Species"
-                                                variant="standard"
-                                                onChange={(e) => setSpecies(e.target.value)}
-                                            />
-                                        </Grid>
-                                        <Grid item xs={12} sm={6} lg={6}>
-                                            <TextField
-                                                style={{ width: "80%" }}
-                                                defaultValue={speciesLatin}
-                                                label="Latin Species"
-                                                variant="standard"
-                                                onChange={(e) => setSpeciesLatin(e.target.value)}
-                                            />
-                                        </Grid>
-                                        <Grid item xs={12} sm={6} lg={6}>
-                                            <TextField
-                                                style={{ width: "80%" }}
-                                                defaultValue={pronouns}
-                                                required
-                                                label="Pronouns"
-                                                variant="standard"
-                                                onChange={(e) => setPronouns(e.target.value)}
-                                            />
-                                        </Grid>
-                                        <Grid item xs={12} sm={6} lg={6}>
-                                            <TextField
-                                                style={{ width: "80%" }}
-                                                defaultValue={web}
-                                                label="Web"
-                                                variant="standard"
-                                                onChange={(e) => setWeb(e.target.value)}
-                                            />
-                                        </Grid>
-
-                                        <Grid item xs={12} sm={6} lg={6}>
-                                            <TextField
-                                                style={{ width: "80%" }}
-                                                defaultValue={birthday}
-                                                required
-                                                label="Birthday"
-                                                variant="standard"
-                                                onChange={(e) => setBirthday(e.target.value)}
-                                            />
-                                        </Grid>
-
-                                        <Grid item lg={12}>
-                                            <Button
-                                                type="file"
-                                                component="label"
-                                                onClick={() => resetValues()}
-                                                startIcon={<RestartAltIcon />}
-                                            >
-                                                Reset
-                                            </Button>
-                                            <Button
-                                                type="file"
-                                                component="label"
-                                                onClick={() => saveFursonaCard()}
-                                                startIcon={<DownloadIcon />}
-                                            >
-                                                Download Card
-                                            </Button>
-                                            <Button
-                                                type="file"
-                                                component="label"
-                                                startIcon={<PhotoCamera />}
-                                            >
-                                                <input
-                                                    type="file"
-                                                    accept="image/*"
-                                                    onChange={handleChange}
-                                                    hidden
-                                                />
-                                                Upload image
-                                            </Button>
-                                        </Grid>
-                                    </Grid>
-                                </Box>
-                            </Grid>
+                    <Grid container >
+                        <Grid item xs={12} sm={12} md={6} lg={5}>
+                            <Card
+                                name={name}
+                                species={species}
+                                speciesLatin={speciesLatin}
+                                pronouns={pronouns}
+                                web={web}
+                                birthday={birthday}
+                                img={file}
+                            />
                         </Grid>
-                    </Paper>
+                        <Grid item xs={12} sm={12} md={6} lg={7}>
+                            <Box
+                                component="form"
+                                sx={{
+                                    "& .MuiTextField-root": {
+                                        m: 1,
+                                        width: "25ch",
+                                    },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <Grid container spacing="2">
+                                    <Grid item xs={12} sm={6} lg={6}>
+                                        <TextField
+                                            style={{ width: "80%" }}
+                                            defaultValue={name}
+                                            required
+                                            label="Name"
+                                            variant="standard"
+                                            onChange={(e) => setName(e.target.value)}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={6} lg={6}>
+                                        <TextField
+                                            style={{ width: "80%" }}
+                                            defaultValue={species}
+                                            required
+                                            label="Species"
+                                            variant="standard"
+                                            onChange={(e) => setSpecies(e.target.value)}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={6} lg={6}>
+                                        <TextField
+                                            style={{ width: "80%" }}
+                                            defaultValue={speciesLatin}
+                                            label="Latin Species"
+                                            variant="standard"
+                                            onChange={(e) => setSpeciesLatin(e.target.value)}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={6} lg={6}>
+                                        <TextField
+                                            style={{ width: "80%" }}
+                                            defaultValue={pronouns}
+                                            required
+                                            label="Pronouns"
+                                            variant="standard"
+                                            onChange={(e) => setPronouns(e.target.value)}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={6} lg={6}>
+                                        <TextField
+                                            style={{ width: "80%" }}
+                                            defaultValue={web}
+                                            label="Web"
+                                            variant="standard"
+                                            onChange={(e) => setWeb(e.target.value)}
+                                        />
+                                    </Grid>
+
+                                    <Grid item xs={12} sm={6} lg={6}>
+                                        <TextField
+                                            style={{ width: "80%" }}
+                                            defaultValue={birthday}
+                                            required
+                                            label="Birthday"
+                                            variant="standard"
+                                            onChange={(e) => setBirthday(e.target.value)}
+                                        />
+                                    </Grid>
+
+                                    <Grid item lg={12}>
+                                        <Button
+                                            type="file"
+                                            component="label"
+                                            onClick={() => resetValues()}
+                                            startIcon={<RestartAltIcon />}
+                                        >
+                                            Reset
+                                        </Button>
+                                        <Button
+                                            type="file"
+                                            component="label"
+                                            onClick={() => saveFursonaCard()}
+                                            startIcon={<DownloadIcon />}
+                                        >
+                                            Download Card
+                                        </Button>
+                                        <Button
+                                            type="file"
+                                            component="label"
+                                            startIcon={<PhotoCamera />}
+                                        >
+                                            <input
+                                                type="file"
+                                                accept="image/*"
+                                                onChange={handleChange}
+                                                hidden
+                                            />
+                                            Upload image
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Grid>
+                    </Grid>
                 </Grid>
 
                 {/* <Grid item xs={12} sm={12} md={8} lg={5}>
@@ -196,7 +194,8 @@ export default function CardCreator() {
                     </Paper>
                 </Grid> */}
             </Grid>
-        </div>
+        </Paper>
+
     );
 }
 

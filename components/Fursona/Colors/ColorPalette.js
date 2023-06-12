08ad5b28b94f6b38d9e8ab517cb6ea6aa0fd3344
tekofx <tekofxx@gmail.com>
2023-06-12 18:46:30 +0200

    Added text and change disposition

--- components/Fursona/Colors/ColorPalette.js
+++ components/Fursona/Colors/ColorPalette.js
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Grid, Typography } from "@mui/material";
 const colors = [
   "#2D2D2D",
   "#A0A0A0",
@@ -18,28 +19,54 @@ export default function ColorPalette(props) {
       props.setSelectedColor("undefined")
     }
   }
+  function getContrastColor(hexcolor) {
+    // If a leading # is provided, remove it
+    if (hexcolor.slice(0, 1) === '#') {
+      hexcolor = hexcolor.slice(1);
+    }
+
+    // Convert to RGB value
+    var r = parseInt(hexcolor.substr(0, 2), 16);
+    var g = parseInt(hexcolor.substr(2, 2), 16);
+    var b = parseInt(hexcolor.substr(4, 2), 16);
+
+    // Get YIQ ratio
+    var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+    // Check contrast
+    return (yiq >= 128) ? '#000000' : '#FFFFFF';
+  }
+
 
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }} >
+    <Grid container
+    >
       {colors.map((color) => (
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 1.1 }}
-          transition={{ type: "spring", stiffness: 400, damping: 20 }}
-          style={{
-            height: 0,
-            width: "12.5%",
-            backgroundColor: color,
-            paddingBottom: "12.5%",
-          }}
-          onMouseOver={() => props.setSelectedColor(color)}
-          onMouseLeave={() => props.setSelectedColor("")}
-          onClick={() => toggleColorChange(color)}
-        />
+        <Grid item lg>
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 20 }}
+            style={{
+              height: "50%",
+              width: "100%",
+              backgroundColor: color,
+              paddingBottom: "50%",
+            }}
+            onMouseOver={() => props.setSelectedColor(color)}
+            onMouseLeave={() => props.setSelectedColor("")}
+            onClick={() => toggleColorChange(color)}
+          >
+            <Typography variant="h6" textAlign="center" style={{ color: getContrastColor(color) }}>
+
+              {color}
+            </Typography>
+          </motion.div>
+        </Grid>
       ))}
+    </Grid>
 
 
 
-    </div>
   );
 }

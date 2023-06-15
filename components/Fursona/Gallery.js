919e82aef3393769c3a93dae5b21e73f2a4a4386
tekofx <tekofxx@gmail.com>
2023-06-15 13:09:00 +0200

    Fixes and added urls

--- components/Fursona/Gallery.js
+++ components/Fursona/Gallery.js
@@ -9,43 +9,32 @@ import LaunchIcon from '@mui/icons-material/Launch';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 const images = [{
     src: process.env.PUBLIC_URL + "img/juaguelin.png",
-    title: "By Teko",
     author: "Teko",
-    description: "",
-    lightBoxTitle: ""
-
+    url: "https://twitter.com/teko_fx"
 }, {
     src: process.env.PUBLIC_URL + "img/among_us.png",
-    title: "By Teko",
     author: "Teko",
-    description: "",
-    lightBoxTitle: ""
+    url: "https://twitter.com/teko_fx"
 },
 {
     src: process.env.PUBLIC_URL + "img/summer - lgtb.jpg",
-    title: "By Teko",
     author: "Teko",
-    description: "",
-    lightBoxTitle: ""
+    url: "https://twitter.com/teko_fx"
 },
 {
     src: process.env.PUBLIC_URL + "img/Merry Foxmas.png",
-    title: "By Teko",
     author: "Teko",
-    description: "",
-    lightBoxTitle: ""
+    url: "https://twitter.com/teko_fx"
 },
 {
     src: process.env.PUBLIC_URL + "img/Autumn.png", title: "By Teko",
-    description: "",
     author: "Teko",
-    lightBoxTitle: ""
+    url: "https://twitter.com/teko_fx"
 },
 {
     src: process.env.PUBLIC_URL + "img/ref-small.jpg", title: "By Teko",
-    description: "",
     author: "Teko",
-    lightBoxTitle: ""
+    url: "https://twitter.com/teko_fx"
 }
 
 ]
@@ -62,71 +51,66 @@ export default function Gallery() {
         setOpen(!open);
     };
 
-
-
-
     const onImageClick = (image) => {
         console.log(image);
         setValue(image);
         setOpen(true);
     };
 
-
-
     return (
-        <Accordion expanded={expanded} onClick={() => setExpanded(!expanded)}>
+        <Accordion expanded={expanded} >
             <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1a-content"
                 id="panel1a-header"
+                onClick={() => setExpanded(!expanded)}
+                sx={{ margin: "10px", cursor: "pointer" }}
             >
                 <Typography variant="h1" textAlign="left">{t.title}</Typography>
             </AccordionSummary>
             <AccordionDetails
             >
-                <Paper sx={{ p: 2 }}>
 
-                    {loading ? (
-                        <Grid
-                            container
-                            spacing={{ xs: 1, sm: 1, md: 1, lg: 1 }}
-                            columns={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }}
-                        >
-                            {Array.from(Array(30).keys()).map((_, i) => (
-                                <Grid item lg={2} key={i}>
-                                    <Skeleton variant="rectangular" sx={{ minHeight: 200 }} />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    ) : (
-                        <div className="gallery">
+                {loading ? (
+                    <Grid
+                        container
+                        spacing={{ xs: 1, sm: 1, md: 1, lg: 1 }}
+                        columns={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }}
+                    >
+                        {Array.from(Array(30).keys()).map((_, i) => (
+                            <Grid item lg={2} key={i}>
+                                <Skeleton variant="rectangular" sx={{ minHeight: 200 }} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                ) : (
+                    <div className="gallery">
 
-                            {images.map((image) => (
-                                <motion.div className="pics"
-                                    whileHover={{ scale: 1.05 }}
-                                    transition={{ type: "spring", stiffness: 400, damping: 20 }}
-                                    onClick={() => onImageClick(image)}
+                        {images.map((image) => (
+                            <motion.div className="pics"
+                                whileHover={{ scale: 1.05 }}
+                                transition={{ type: "spring", stiffness: 400, damping: 20 }}
+                                onClick={() => onImageClick(image)}
+                            >
+                                <img src={image.src} alt={image.title} className="pic" />
+                                <Paper className="text"
+                                    component={motion.div}
+                                    whileHover={{ backgroundColor: Theme.palette.primary.main }}
+                                    transition={{
+                                        ease: "easeIn",
+                                        duration: 0.2,
+                                    }}
                                 >
-                                    <img src={image.src} alt={image.title} className="pic" />
-                                    <Paper className="text"
-                                        component={motion.div}
-                                        whileHover={{ backgroundColor: Theme.palette.primary.main }}
-                                        transition={{
-                                            ease: "easeIn",
-                                            duration: 0.2,
-                                        }}
-                                    >
-                                        <Stack direction="row" spacing={1}>
-                                            <Typography>Art by {image.author}</Typography>
-                                            <LaunchIcon />
-                                        </Stack>
-                                    </Paper>
-                                </motion.div>
-                            ))}
-                        </div>
+                                    <Stack direction="row" spacing={1} onClick={() => window.open(image.url, "_blank")}>
+                                        <Typography>Art by {image.author}</Typography>
+                                        <LaunchIcon />
+                                    </Stack>
+                                </Paper>
+                            </motion.div>
+                        ))}
+                    </div>
 
-                    )}
-                </Paper>
+                )}
             </AccordionDetails>
 
             <Dialog open={open} onClose={toggleOpen} maxWidth="lg" PaperProps={{
